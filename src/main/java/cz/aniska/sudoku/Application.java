bde37f560cd9b30b4bc838fa4a5f7a10988ccb7c
Alex Aniska <aniska88@gmail.com>
2016-10-10 01:55:24 +0200

    App maven configuration

---
+++ src/main/java/cz/aniska/sudoku/Application.java
@@ -0,0 +1,16 @@
+package cz.aniska.sudoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aliaksandr Aniska
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
