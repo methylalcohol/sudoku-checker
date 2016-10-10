abd9fcaee6588516d17e1dc78c8ba1af28117489
Alex Aniska <aniska88@gmail.com>
2016-10-10 03:41:49 +0200

    Fixed bug with boolean inversion. Some variables was extracted to constants

--- src/main/java/cz/aniska/sudoku/service/SudokuCheckerServceImpl.java
+++ src/main/java/cz/aniska/sudoku/service/SudokuCheckerServceImpl.java
@@ -16,6 +16,8 @@ import java.util.Set;
 @Component
 public class SudokuCheckerServceImpl implements SudokuCheckerService {
 
+    private final int BOARD_SIZE = 9;
+    private final int SQUARE_SIDE_LEN = (int) Math.sqrt(BOARD_SIZE);
     /**
      *
      * @param board
@@ -23,23 +25,22 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
      */
     @Override
     public SudokuCheckerStatus checkSudokuBoard(final int[][] board) {
-        //we do 9 iterations as we have 9 rows, 9 columns and 9 squares
+        boolean emptyBoxFoundFlag = false;
 
-        boolean someBoxEmptyEmptyFlag = false;
-
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
             //row is already known as it is first index of 2D array
             final int[] row = board[i];
             //column should be constructed by iteration through the rows
-            final int[] column = new int[9];
+            final int[] column = new int[BOARD_SIZE];
             //square that will become an 1D array.
-            final int[] square = new int[9];
+            final int[] square = new int[BOARD_SIZE];
 
-            for (int rowIndex = 0; rowIndex < 9; rowIndex ++) {
+            for (int rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex ++) {
                 //building column
                 column[rowIndex] = board[rowIndex][i];
                 //unbend the square, we re-map i-square to 1D array
-                square[rowIndex] = board[(i / 3) * 3 + rowIndex / 3][i * 3 % 9 + rowIndex % 3];
+                square[rowIndex] = board[(i / SQUARE_SIDE_LEN) * SQUARE_SIDE_LEN + rowIndex / SQUARE_SIDE_LEN]
+                                        [i * SQUARE_SIDE_LEN % BOARD_SIZE + rowIndex % SQUARE_SIDE_LEN];
             }
 
             //validation results
@@ -48,7 +49,7 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
             CheckNumbersResult checkSquare = validateParticularRowColumnOrSquare(square);
 
             //check whether sudoku can be potentially finished
-            someBoxEmptyEmptyFlag = checkRowResult.isSomeBoxEmpty() |
+            emptyBoxFoundFlag = checkRowResult.isSomeBoxEmpty() |
                     checkColumnResult.isSomeBoxEmpty() | checkSquare.isSomeBoxEmpty();
 
             if (!checkRowResult.isNumbersAreValid() ||
@@ -56,9 +57,11 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
                         !checkSquare.isNumbersAreValid()) {
 
                 return SudokuCheckerStatus.NOT_VALID_MOVE;
-            } else if (someBoxEmptyEmptyFlag) {
-                return SudokuCheckerStatus.SUDOKU_FINISHED;
             }
+        }
+
+        if (!emptyBoxFoundFlag) {
+            return SudokuCheckerStatus.SUDOKU_FINISHED;
         }
 
         return SudokuCheckerStatus.SUCCESS_MOVE;
@@ -80,11 +83,11 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
             //empty boxes are valid (zero), so we check can jump to the next one
             if (number == 0) {
                 emptyBoxDetected = true;
-                break;
+                continue;
             }
 
             //validate numbers in boxes
-            if (number <= 1 || number > 9 || !intSetChecker.add(number)) {
+            if (number < 0 || number > BOARD_SIZE || !intSetChecker.add(number)) {
                 return new CheckNumbersResult(emptyBoxDetected, false);
             }
         }
@@ -121,9 +124,9 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
      *
      * @author Aliaksandr Aniska
      */
-    private class CheckNumbersResult {
-        private boolean someBoxEmpty;
-        private boolean numbersAreValid;
+    private final class CheckNumbersResult {
+        private final boolean  someBoxEmpty;
+        private final boolean  numbersAreValid;
 
         public CheckNumbersResult(boolean someBoxEmpty, boolean numbersAreValid) {
             this.someBoxEmpty = someBoxEmpty;
@@ -131,11 +134,11 @@ public class SudokuCheckerServceImpl implements SudokuCheckerService {
         }
 
         public boolean isSomeBoxEmpty() {
-            return someBoxEmpty;
+            return this.someBoxEmpty;
         }
 
         public boolean isNumbersAreValid() {
-            return numbersAreValid;
+            return this.numbersAreValid;
         }
     }
 
